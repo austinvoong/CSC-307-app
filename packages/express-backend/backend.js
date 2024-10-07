a053ad889738614a86bfd4e80a7d8022ec8f66f7
Austin Voong <anvoong@calpoly.edu>
2024-10-06 20:21:35 -0700

    users by id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -34,11 +34,16 @@ const users = {
     ]
   };
 
+  const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
+  
   const findUserByName = (name) => {
     return users["users_list"].filter(
       (user) => user["name"] === name
     );
   };
+
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -55,6 +60,16 @@ app.get("/users", (req, res) => {
     res.send(users);
   }
 });
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+      res.status(404).send("Resource not found.");
+    } else {
+      res.send(result);
+    }
+  });
 
 app.listen(port, () => {
   console.log(
